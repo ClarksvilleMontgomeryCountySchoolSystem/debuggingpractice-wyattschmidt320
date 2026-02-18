eb1099dc4dc7538f1c1e9510afe91c4ad09fd804
Wyatt Schmidt <wyatt.schmidt320@students.cmcss.net>
2026-02-18 11:55:10 -0600

    utghbjn

--- task1.py
+++ task1.py
@@ -9,13 +9,13 @@
 
 # --- Snippet 1 ---
 # This should display a greeting
-message = "Hello, welcome to Python!
+message = "Hello, welcome to Python!"
 print(message)
 
 # --- Snippet 2 ---
 # This should display a student's name
 student_name = "Jordan"
-print(f"The student is {studentname}")
+print(f"The student is {student_name}")
 
 # --- Snippet 3 ---
 # This should display a favorite color
@@ -24,105 +24,105 @@ print (f"My favorite color is {favorite_color}")
 
 # --- Snippet 4 ---
 # This should display a city name
-city = Austin
+city = "Austin"
 print(f"I live in {city}")
 
 # --- Snippet 5 ---
 # This should display the number of pets
-number of pets = 5
+number_of_pets = 5
 print(f"I have {number_of_pets} pets")
 
 # --- Snippet 6 ---
 # This should display a book title
-book_title = "The Great Gatsby'
+book_title = "The Great Gatsby"
 print(f"I am reading {book_title}")
 
 # --- Snippet 7 ---
 # This should display a greeting
 greeting = "Good morning!"
-  print(f"{greeting}")
+print(f"{greeting}")
 
 # --- Snippet 8 ---
 # This should display a temperature
 temperature = 72
-print(f"The temperature is {temperture} degrees")
+print(f"The temperature is {temperature} degrees")
 
 # --- Snippet 9 ---
 # This should display a team name
 team_name = "Eagles"
-print f"Go {team_name}!"
+print (f"Go {team_name}!")
 
 # --- Snippet 10 ---
 # This should display a food item
 favorite_food = "pizza"
-print(f"I love (favorite_food}")
+print(f"I love {favorite_food}")
 
 # --- Snippet 11 ---
 # This should display an animal
 animal = "dog"
-print(f"My favorite animal is {animal}')
+print(f"My favorite animal is {animal}")
 
 # --- Snippet 12 ---
 # This should display a school name
-school name = "Central High"
+school_name = "Central High"
 print(f"I attend {school_name}")
 
 # --- Snippet 13 ---
 # This should display a movie title
-movie_title = Inception
+movie_title = "Inception"
 print(f"My favorite movie is {movie_title}")
 
 # --- Snippet 14 ---
 # This should display a fruit
 fruit = "apple"
-print(f"I like to eat {frut}")
+print(f"I like to eat {fruit}")
 
 # --- Snippet 15 ---
 # This should display a sport
 favorite_sport = "basketball"
-   print(f"I play {favorite_sport}")
+print(f"I play {favorite_sport}")
 
 # --- Snippet 16 ---
 # This should display a greeting
 morning_greeting = "Rise and shine!"
-print(f{morning_greeting})
+print(f"{morning_greeting}")
 
 # --- Snippet 17 ---
 # This should display a car color
 car_color = "red"
-print(f"My car is {car_color})
+print(f"My car is {car_color}")
 
 # --- Snippet 18 ---
 # This should display a day
-3rd_day = "Wednesday"
-print(f"Today is {3rd_day}")
+third_day = "Wednesday"
+print(f"Today is {third_day}")
 
 # --- Snippet 19 ---
 # This should display a state
 home_state = "Texas"
-prnt(f"I live in {home_state}")
+print(f"I live in {home_state}")
 
 # --- Snippet 20 ---
 # This should display a season
-favorite_season = 'summer"
+favorite_season = "summer"
 print(f"My favorite season is {favorite_season}")
 
 # --- Snippet 21 ---
 # This should check if the weather is hot
 temperature = 95
-if temperature > 90
+if temperature > 90:
     print("It is hot outside!")
 
 # --- Snippet 22 ---
 # This should check if a student passed
 score = 85
 if score >= 70:
-print("You passed!")
+    print("You passed!")
 
 # --- Snippet 23 ---
 # This should display a message based on age
 age = 18
 if age >= 18:
     print("You are an adult.")
-else
+else:
     print("You are a minor.")
--- task2.py
+++ task2.py
@@ -26,53 +26,53 @@ def main():
     soda_quantity_str = input("How many sodas? ")
     gallons_str = input("How many gallons of gas? ")
     pounds_str = input("How many pounds of deli meat? ")
-    lottery_quantity_str = input("How many lottery tickets? )
+    lottery_quantity_str = input("How many lottery tickets? ")
 
 
 # Comment out the line below after fixing input bugs
-main()
+#main()
 
 
 # --- Receipt Header ---
-print(""
+print("""
 ========================================
        GAS STATION RECEIPT
       Thank you for shopping!
 ========================================
-"")
+""")
 
 # --- Candy Bars ---
 # Price: $1.89 each (float price, int quantity)
 candy_price = 1.89
-candy_quantity = candy_quantity_str
+candy_quantity = int(candy_quantity_str)
 candy_total = candy_price * candy_quantity
 print(f"Candy Bars: {candy_quantity} x ${candy_price} = ${candy_total}")
 
 # --- Soda Bottles ---
 # Price: $2.49 each (float price, int quantity)
 soda_price = 2.49
-soda_quantity = soda_quantity_str
+soda_quantity = int(soda_quantity_str)
 soda_total = soda_price * soda_quantity
 print(f"Soda: {soda_quantity} x ${soda_price} = ${soda_total}")
 
 # --- Gas ---
 # Price: $3.25 per gallon (float price, float quantity)
 gas_price = 3.25
-gallons = gallons_str
+gallons = float(gallons_str)
 gas_total = gas_price * gallons
 print(f"Gas: {gallons} gallons x ${gas_price} = ${gas_total}")
 
 # --- Deli Meat ---
 # Price: $8.99 per pound (float price, float quantity)
 deli_price = 8.99
-pounds = pounds_str
+pounds = float(pounds_str)
 deli_total = deli_price * pounds
 print(f"Deli Meat: {pounds} lbs x ${deli_price} = ${deli_total}")
 
 # --- Lottery Tickets ---
 # Price: $2 each (int price, int quantity)
 lottery_price = 2
-lottery_quantity = lottery_quantity_str
+lottery_quantity = int(lottery_quantity_str)
 lottery_total = lottery_price * lottery_quantity
 print(f"Lottery: {lottery_quantity} x ${lottery_price} = ${lottery_total}")
 
